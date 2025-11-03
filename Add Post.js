6108614c45c593f00df2b7254349a28f56cfaf07
bodiemorgan <bodiemorgan1@gmail.com>
2025-11-03 22:06:58 +0000

    Add files via upload

---
+++ Add Post.js
@@ -0,0 +1,4 @@
+<h2>Add a Blog Post</h2>
+<input type="text" id="postTitle" placeholder="Title">
+<textarea id="postContent" placeholder="Content"></textarea>
+<button onclick="addPost()">Add Post</button>
